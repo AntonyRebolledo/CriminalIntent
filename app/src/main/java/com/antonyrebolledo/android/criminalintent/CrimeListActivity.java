35b06aed9183cb34d9328750180a7c436664af41
Antony Rebolledo <tony_rebo@hotmail.com>
2018-12-13 15:53:36 -0600

    CriminalIntent: Muestra una lista simple de crimenes.

---
+++ app/src/main/java/com/antonyrebolledo/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.antonyrebolledo.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
